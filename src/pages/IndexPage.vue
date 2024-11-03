6de818648cb7fd719b182e11149d5769420ec95d
ewgene <ewgene.shure@gmail..com>
2024-11-03 19:39:53 +0700

    init

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,15 +1,25 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+  <q-page class="flex flex-center"> </q-page>
 </template>
 
 <script setup>
 defineOptions({
-  name: 'IndexPage'
+  name: "IndexPage",
+});
+
+import { ref, onMounted, computed } from "vue";
+import { usePointsStore } from "../stores/points";
+const store = usePointsStore();
+
+const getPoints = computed(() => {
+  return store.getPoints;
+});
+
+const points = computed(() => {
+  return store.points;
+});
+
+onMounted(() => {
+  store.fetchPoints();
 });
 </script>
---
+++ src/stores/points.js
@@ -0,0 +1,18 @@
+import { defineStore } from "pinia";
+import pointsLib from "src/lib/pointsLib.json";
+
+export const usePointsStore = defineStore("points", {
+  state: () => ({
+    points: [],
+  }),
+  getters: {
+    getPoints(state) {
+      return state.points;
+    },
+  },
+  actions: {
+    async fetchPoints() {
+      this.points = pointsLib;
+    },
+  },
+});
